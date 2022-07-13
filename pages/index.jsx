8fda3d800b75b69aef63499b7566c9e6ebc690e6
Vinicius Jara <81604619+vf-jara@users.noreply.github.com>
2022-07-13 10:26:36 -0400

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -27,14 +27,14 @@ export default function Home() {
 
                 <div className=" px-16 pt-20 w-full ">
                     <h2 className="px-4 text-2xl font-bold text-white pb-7">Conteúdos Recentes</h2>
-                    <SliderPadrão cursos={cursos} />
+                    <SliderPadrao cursos={cursos} />
                 </div>
 
             </div>
 
             <div className="pt-40 px-16">
                 <h2 className="px-4 text-2xl font-bold text-white pb-7">Cursos Completos</h2>
-                <SliderPadrão cursos={cursos} />
+                <SliderPadrao cursos={cursos} />
             </div>
 
         </div>

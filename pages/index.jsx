f8cd407b132729667f19140f62bf821f0089baba
Bernardo <eduardo.bernardo@indexdc.com>
2022-07-14 11:57:21 -0400

    Informações da Homepage populando nos campos corretamente

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
 import { getSession } from "next-auth/react";
+import Head from "next/head";
 import { useRouter } from 'next/router';
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import SliderPadrao from "../components/Slider";
 import { homeInfo } from "../lib/api";
 
-export default function Home({ session, courses }) {
+export default function Home({ session, courses, homepageInfo }) {
     const router = useRouter();
     const [cursos, setCursos] = useState([]);
 
@@ -13,40 +14,50 @@ export default function Home({ session, courses }) {
         setCursos(courses);
     }, [])
     return (
-        <div className="bg-[#000303] pb-16">
-            <div className="bg-home bg-top-center bg-no-repeat bg-cover">
-                <Header />
-                <div className="md:w-2/3 pt-32 md:pt-16 px-8 md:px-20 2xl:pb-20">
-                    <h1 className="text-white font-bold text-5xl md:text-6xl pb-4">
-                        As bases da IS
-                    </h1>
-                    <p className="text-white text-xl md:text-lg w-4/5 mb-12">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra condimentum justo, sed dapibus eros varius non. Ut condimentum porta ligula, at suscipit nibh dapibus a. In semper mauris sed eros blandit, a viverra diam interdum. Donec in ipsum mauris. Sed eu mi consequat, scelerisque erat eget, viverra diam.
-                    </p>
-                    <button className="text-base flex align-middle ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 text-white px-10 pt-[16px] pb-[17px] bg-orange-is hover:bg-white hover:text-orange-is justify-center rounded-md font-medium">
-                        Assistir Agora
-                        <svg className="w-5 h-5 ml-2 -mr-1 mt-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </button>
+        <>
+            <Head>
+                <title>{homepageInfo?.seoTitle || 'Dashboard'}</title>
+                <meta name="description" content={homepageInfo?.seoDescription || 'Description'} />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className="bg-[#000303] pb-16">
+                <div className="bg-home bg-top-center bg-no-repeat bg-cover">
+                    <Header />
+                    <div className="md:w-2/3 pt-32 md:pt-16 px-8 md:px-20 2xl:pb-20">
+                        <h1 className="text-white font-bold text-5xl md:text-6xl pb-4">
+                            {homepageInfo?.heroTitle || '[Hero Title] As bases da IS'}
+                        </h1>
+                        <p className="text-white text-xl md:text-lg w-4/5 mb-12">
+                            {homepageInfo?.heroDescription || '[Hero Description] Lorem Ipsum'}
+                        </p>
+                        <button 
+                            className="text-base flex align-middle ease-in-out hover:-translate-y-1 hover:scale-105 duration-300 text-white px-10 pt-[16px] pb-[17px] bg-orange-is hover:bg-white hover:text-orange-is justify-center rounded-md font-medium"
+                            onClick={() => {router.push(homepageInfo?.heroButtonLink || '/')}}
+                        >
+                            {homepageInfo?.heroButtonText || '[Button] Assistir'}
+                            <svg className="w-5 h-5 ml-2 -mr-1 mt-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </button>
+                    </div>
+
+                    <div className="px-5 md:px-16 pt-16 w-full">
+                        <h2 className="px-4 text-2xl font-bold text-white pb-7">Conteúdos Recentes</h2>
+                        {cursos && (
+                            <SliderPadrao cursos={cursos} />
+                        )}
+                    </div>
+
                 </div>
 
-                <div className="px-5 md:px-16 pt-16 w-full">
-                    <h2 className="px-4 text-2xl font-bold text-white pb-7">Conteúdos Recentes</h2>
+                <div className="pt-16 px-5 md:px-16 w-full">
+                    <h2 className="px-4 text-2xl font-bold text-white pb-7">Cursos Completos</h2>
                     {cursos && (
                         <SliderPadrao cursos={cursos} />
+
                     )}
                 </div>
 
             </div>
-
-            <div className="pt-16 px-5 md:px-16 w-full">
-                <h2 className="px-4 text-2xl font-bold text-white pb-7">Cursos Completos</h2>
-                {cursos && (
-                    <SliderPadrao cursos={cursos} />
-
-                )}
-            </div>
-
-        </div>
+        </>
     )
 }
 

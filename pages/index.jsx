61b615137ad123a9f9bd947528bbf7ce1cdcbb19
Bernardo <eduardo.bernardo@indexdc.com>
2022-07-14 12:02:32 -0400

    Homepage background Image populando via backend

--- pages/index.jsx
+++ pages/index.jsx
@@ -21,7 +21,7 @@ export default function Home({ session, courses, homepageInfo }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <div className="bg-[#000303] pb-16">
-                <div className="bg-home bg-top-center bg-no-repeat bg-cover">
+                <div className="bg-top-center bg-no-repeat bg-cover" style={{backgroundImage: `url(${homepageInfo?.heroImage?.data?.attributes?.url})`}}>
                     <Header />
                     <div className="md:w-2/3 pt-32 md:pt-16 px-8 md:px-20 2xl:pb-20">
                         <h1 className="text-white font-bold text-5xl md:text-6xl pb-4">

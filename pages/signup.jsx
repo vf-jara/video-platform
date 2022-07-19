e452a888b1a9e626a5de084358df0cccc9218425
Vinicius Jara <viniciusfjara@gmail.com>
2022-07-19 15:16:28 -0400

    fix de svg

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -17,7 +17,7 @@ export default function SignUp() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    await axios.post("/api/signup", {name: enterName, email: enterMail, password: enterPassword}).then((response) => {
+    await axios.post("/api/signup", { name: enterName, email: enterMail, password: enterPassword }).then((response) => {
       console.log(response.data);
       const { redirectUrl } = response.data;
       if (redirectUrl) {
@@ -30,8 +30,8 @@ export default function SignUp() {
     });
   };
 
-  useEffect(()=> {
-    if(router.query.error){
+  useEffect(() => {
+    if (router.query.error) {
       setError(router.query.error);
     }
   }, []);
@@ -44,34 +44,34 @@ export default function SignUp() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {error && error === 'notauthorized' ? (
-          <div id="toast-danger" className="animate-fade-in-down absolute top-5 right-5 flex items-center p-4 mb-4 w-full max-w-xs sm:max-w-lg text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 z-50" role="alert">
-              <div className="inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
-                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-              </div>
-              <div className="ml-3 text-base sm:text-lg font-normal">
-                <strong className="font-bold">Ooops! </strong>
-                <span className="block sm:inline">Você precisa estar logado para acessar a plataforma.</span>
-              </div>
-              <button type="button" onClick={()=> {setError("")}} className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
-                  <span className="sr-only">Close</span>
-                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-              </button>
+        <div id="toast-danger" className="animate-fade-in-down absolute top-5 right-5 flex items-center p-4 mb-4 w-full max-w-xs sm:max-w-lg text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 z-50" role="alert">
+          <div className="inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
           </div>
-        ): error === 'invalid' && (
-          <div id="toast-danger" className="animate-fade-in-down absolute top-5 right-5 flex items-center p-4 mb-4 w-full max-w-xs sm:max-w-lg text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 z-50" role="alert">
-              <div className="inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
-                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-              </div>
-              <div className="ml-3 text-base sm:text-lg font-normal">
-                <strong className="font-bold">Ooops! </strong>
-                <span className="block sm:inline">Usuário ou senha inválidos.</span>
-              </div>
-              <button type="button" onClick={()=> {setError("")}} className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
-                  <span className="sr-only">Close</span>
-                  <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-              </button>
+          <div className="ml-3 text-base sm:text-lg font-normal">
+            <strong className="font-bold">Ooops! </strong>
+            <span className="block sm:inline">Você precisa estar logado para acessar a plataforma.</span>
           </div>
-        )}
+          <button type="button" onClick={() => { setError("") }} className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
+            <span className="sr-only">Close</span>
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+          </button>
+        </div>
+      ) : error === 'invalid' && (
+        <div id="toast-danger" className="animate-fade-in-down absolute top-5 right-5 flex items-center p-4 mb-4 w-full max-w-xs sm:max-w-lg text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800 z-50" role="alert">
+          <div className="inline-flex flex-shrink-0 justify-center items-center w-8 h-8 text-red-500 bg-red-100 rounded-lg dark:bg-red-800 dark:text-red-200">
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+          </div>
+          <div className="ml-3 text-base sm:text-lg font-normal">
+            <strong className="font-bold">Ooops! </strong>
+            <span className="block sm:inline">Usuário ou senha inválidos.</span>
+          </div>
+          <button type="button" onClick={() => { setError("") }} className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-dismiss-target="#toast-danger" aria-label="Close">
+            <span className="sr-only">Close</span>
+            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+          </button>
+        </div>
+      )}
       <div className="hidden md:block h-screen relative md:col-span-8">
         <Img
           src="/assets/images/fundo-2.jpg"
@@ -81,7 +81,7 @@ export default function SignUp() {
       <div className="md:col-span-4 h-screen px-8 2xl:px-5 align-middle">
         <div className="w-full flex items-center justify-center">
           <div className="w-full h-screen flex flex-col justify-center items-center align-middle">
-            <Img src="/assets/images/logo.png" style={{height: '100px'}} />
+            <Img src="/assets/images/logo.png" style={{ height: '100px' }} />
             <p className="text-base text-center 2xl:text-lg text-blue-is font-semibold mt-8">
               Preencha os dados abaixo para criar a sua conta
             </p>
@@ -89,9 +89,9 @@ export default function SignUp() {
               className="w-full mb-8 mt-5 2xl:mb-16"
               onSubmit={handleSubmit}
             >
-                <label htmlFor="name" className="block font-bold text-sm 2xl:text-lg text-blue-is">
-                    Qual o seu nome?
-                </label>
+              <label htmlFor="name" className="block font-bold text-sm 2xl:text-lg text-blue-is">
+                Qual o seu nome?
+              </label>
               <input
                 type="text"
                 name="name"
@@ -139,7 +139,7 @@ export default function SignUp() {
                   className="flex items-center justify-center text-center text-base 2xl:text-lg w-full px-6 py-4 bg-orange-is rounded-lg text-white font-bold hover:bg-orange-hv-is transition-colors"
                 >
                   <svg className="animate-spin h-7 w-7 mr-5 flex" fill="transparent" viewBox="0 0 24 24">
-                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                   </svg>
                   Aguarde...
@@ -156,14 +156,14 @@ export default function SignUp() {
             </form>
             <p className="mb-5 text-blue-is text-center text-base 2xl:text-lg">
               Já possui uma conta?
-              <br/>
+              <br />
               <Link href={{
-                    pathname: '/login'
-                }}>
+                pathname: '/login'
+              }}>
                 <a>
-                    <button className="text-orange-is underline hover:font-bold transition-all">
-                        Clique aqui para entrar!
-                    </button>
+                  <button className="text-orange-is underline hover:font-bold transition-all">
+                    Clique aqui para entrar!
+                  </button>
                 </a>
               </Link>
             </p>
@@ -178,13 +178,13 @@ export default function SignUp() {
 
 export const getServerSideProps = async ({ req }) => {
   const session = await getSession({ req });
-  if(session){
-    return{
+  if (session) {
+    return {
       redirect: {
         destination: '/'
       }
     }
-  }else{
+  } else {
     return {
       props: {},
     }

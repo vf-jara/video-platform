fac5f89f4e1494ec7514b6f626f9ea1f2350fbf9
Vinicius Jara <viniciusfjara@gmail.com>
2022-07-21 13:02:37 -0400

    modal mobile

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -87,23 +87,23 @@ export default function SliderPadrao({ cursos }) {
                     </div>
                 ))}
             </Slider>
-            <div className={`fixed p-10 top-0 left-0 bottom-0 right-0 w-full h-full flex items-center justify-center ${modalOpen ? '' : 'hidden'} backdrop-blur`}>
-                <div className="bg-zinc-900 rounded-lg p-6 relative min-w-[50%] max-w-3xl">
+            <div className={`fixed lg:p-10 top-0 left-0 bottom-0 right-0 w-full h-full flex items-end lg:items-center lg:justify-center ${modalOpen ? '' : 'hidden'} backdrop-blur`}>
+                <div className="bg-zinc-900 rounded-lg p-6 relative min-w-[50%] max-w-3xl modal-animation-fade">
                     <button onClick={() => setOpen(!modalOpen)} className="absolute top-5 right-5 text-white">
                         <X size={30} weight="bold" className="" />
                     </button>
-                    <div className="flex gap-x-10">
+                    <div className="flex gap-x-3 lg:gap-x-10 py-5">
                         <div className="min-w-[30%]">
                             <Img
                                 src={info?.image}
                             />
                         </div>
                         <div className="flex flex-col w-full">
-                            <h2 className="text-4xl text-white mb-3">{info?.title}</h2>
-                            <p className="text-white opacity-75 text-base xl:text-lg mb-8">{info?.description}</p>
+                            <h2 className="text-lg lg:text-4xl text-white mb-3">{info?.title}</h2>
+                            <p className="text-white opacity-75 text-sm xl:text-lg mb-8">{info?.description}</p>
                             <Link href={`/${info?.link}`}>
                                 <a>
-                                    <div className="p-4 flex text-white text-lg rounded-lg gap-3 bg-orange-is hover:bg-orange-hv-is w-2/5 justify-center">
+                                    <div className="p-4 flex text-white text-lg rounded-lg gap-3 bg-orange-is hover:bg-orange-hv-is transition-colors lg:w-2/5 justify-center">
                                         <p>Assistir</p>
                                         <Play size={24} weight="fill"/>
                                     </div>
